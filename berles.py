ee28ac83a7018cefca6683a085032894a1b34cef
probbi <rpinter@protonmail.com>
2026-04-14 18:40:09 +0200

    Bérlés osztály hozzáadva

--- berles.py
+++ berles.py
@@ -6,3 +6,15 @@ class Berles
     def __init__(self, auto: Auto, datum: date) -> None:
         self.__auto: Auto = auto
         self.__datum: date = datum
+
+
+    @property
+    def auto(self) -> Auto:
+        return self.__auto
+
+    @property
+    def datum(self) -> date:
+        return self.__datum
+
+    def __str__(self) -> str:
+        return f"{self.__datum} - {self.__auto.rendszam} ({self.__auto.tipus})"
